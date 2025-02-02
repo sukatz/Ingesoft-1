9b699b0835a62b60d5d06410d1afecc5853776a7
itsLisis <feduque@unal.edu.co>
2025-02-02 16:02:40 -0500

    Mejora GUI

---
+++ Proyecto/src/pages/Searching.js
@@ -0,0 +1,13 @@
+// Searching.js
+import React from 'react';
+
+const Searching = () => {
+    return (
+        <div>
+            <h2>Buscando partida...</h2>
+            <p>Estamos buscando un oponente para ti. Por favor espera un momento.</p>
+        </div>
+    );
+};
+
+export default Searching;
