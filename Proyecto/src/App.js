e78feba007e9ca054b9c234e004f4b641073a2c5
itsLisis <feduque@unal.edu.co>
2025-02-02 10:54:48 -0500

    Implementación Textfield conectado a la DB

--- Proyecto/src/App.js
+++ Proyecto/src/App.js
@@ -1,34 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-import {Prueba} from './components/primerComponente'; // Ajusta la ruta según corresponda
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-      
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Prueba/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          xdddd
-        </a>
-        
-      </header>
-    </div>
-
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />  {/* Esta ruta hace que Home sea la página principal */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
